f8e4ac45861beef9177ffd3416c12b80cff9fb4e
linux_china <libing.chen@gmail.com>
2019-11-05 21:52:15 -0800

    add ConnectionSetupPayload

--- lib/rsocket/payload.rb
+++ lib/rsocket/payload.rb
@@ -15,6 +15,7 @@ module RSocket
     def metadata
       @metadata
     end
+
     # @return [String, nil] the contents of our object or nil
     def metadata_utf8
       @metadata.nil? ? nil : @metadata.pack('c*')
@@ -31,10 +32,23 @@ module RSocket
     end
   end
 
+  class ConnectionSetupPayload
+    attr_accessor :metadata_mime_type, :data_mime_type, :metadata, :data
+
+    def initialize(metadata_mime_type = "text/plain", data_mime_type = "text/plain", data, metadata)
+      @metadata_mime_type = metadata_mime_type
+      @data_mime_type = data_mime_type
+      @data = data
+      @metadata = metadata
+    end
+  end
+
 end
 
-# @param data [Array<Byte>] data array
-# @param metadata [Array<Byte>] metadata array
+# @param data [Array<Byte>, String]
+# @param metadata [Array<Byte>, String]
 def payload_of(data, metadata)
-  RSocket::Payload.new(data, metadata)
+  data_bytes = !data.nil? && data.is_a?(String) ? data.unpack('C*') : data
+  metadata_bytes = !metadata.nil? && metadata.is_a?(String) ? metadata.unpack('C*') : metadata
+  RSocket::Payload.new(data_bytes, metadata_bytes)
 end
