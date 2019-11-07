8cb86c203320055fe3f0749baeefe00710972304
linux_china <libing.chen@gmail.com>
2019-11-07 01:43:42 -0800

    add yard dooc

--- lib/rsocket/abstract_rsocket.rb
+++ lib/rsocket/abstract_rsocket.rb
@@ -7,6 +7,7 @@ module RSocket
 
     attr_accessor :onclose
 
+    #@param payload [RSocket::Payload]
     def fire_and_forget(payload)
       raise 'fire_and_forget not implemented'
     end
@@ -17,15 +18,19 @@ module RSocket
       raise 'request_response not implemented'
     end
 
+    #@param payload [RSocket::Payload]
+    #@return [Rx::Observable]
     def request_stream(payload)
       raise 'request_stream not implemented'
     end
 
+    #@@param payloads [Rx::Observable]
     def request_channel(payloads)
       raise 'request_channel not implemented'
     end
 
-    def metadata_push(payloads)
+    #@param payload [RSocket::Payload]
+    def metadata_push(payload)
       raise 'metadata_push not implemented'
     end
 
