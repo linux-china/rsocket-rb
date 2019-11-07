a834184fa65862fde25a2c1d6485646a5485af66
linux_china <libing.chen@gmail.com>
2019-11-07 13:42:56 -0800

    adjust default return value

--- lib/rsocket/abstract_rsocket.rb
+++ lib/rsocket/abstract_rsocket.rb
@@ -9,29 +9,29 @@ module RSocket
 
     #@param payload [RSocket::Payload]
     def fire_and_forget(payload)
-      raise 'fire_and_forget not implemented'
+      puts 'fire_and_forget not implemented'
     end
 
     #@param payload [RSocket::Payload]
     #@return [Rx::Observable]
     def request_response(payload)
-      raise 'request_response not implemented'
+      Rx::Observable.raise_error("request_response not implemented")
     end
 
     #@param payload [RSocket::Payload]
     #@return [Rx::Observable]
     def request_stream(payload)
-      raise 'request_stream not implemented'
+      Rx::Observable.raise_error("request_stream not implemented")
     end
 
     #@@param payloads [Rx::Observable]
     def request_channel(payloads)
-      raise 'request_channel not implemented'
+      Rx::Observable.raise_error("request_channel not implemented")
     end
 
     #@param payload [RSocket::Payload]
     def metadata_push(payload)
-      raise 'metadata_push not implemented'
+      puts 'metadata_push not implemented'
     end
 
     def dispose
