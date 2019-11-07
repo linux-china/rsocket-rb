d7f6b9a7c0230ee26c77908536ad3714bdeb9e2a
linux_china <libing.chen@gmail.com>
2019-11-06 21:47:14 -0800

    implement request

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require './lib/rsocket/responder'
 require './lib/rsocket/payload'
+require 'rx'
 
 set :schema, 'tcp'
 set :port, 42252
@@ -8,7 +9,17 @@ set :port, 42252
 # RSocket request/response
 # @param payload [Payload] rsocket payload
 def request_response(payload)
-  print "RPC called"
-  payload_of("data", "metadata")
+  puts "request/response called"
+  Rx::Observable.just(payload_of("data", "metadata"))
 end
 
+
+def request_stream(payload)
+  print "request/stream called"
+  Rx::Observable.from_array([payload_of("first", "metadata"), payload_of("second", "metadata")])
+end
+
+
+def fire_and_forget(payload)
+  print "fire_and_forget"
+end
