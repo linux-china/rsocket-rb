46daf5f931e64644915074e9549989bad060d7b6
linux_china <libing.chen@gmail.com>
2019-11-07 11:18:48 -0800

    adjust server code

--- server.rb
+++ server.rb
@@ -1,25 +1,27 @@
-require './lib/rsocket/responder'
-require './lib/rsocket/payload'
+require 'rsocket/server_bootstrap'
+require 'rsocket/payload'
 require 'rx'
 
 set :schema, 'tcp'
 set :port, 42252
 
 
-# RSocket request/response
-# @param payload [Payload] rsocket payload
+# @param payload [RSocket::Payload]
+#@return [Rx::Observable]
 def request_response(payload)
   puts "request/response called"
   Rx::Observable.just(payload_of("data", "metadata"))
 end
 
-
+# @param payload [RSocket::Payload]
+#@return [Rx::Observable]
 def request_stream(payload)
   print "request/stream called"
   Rx::Observable.from_array([payload_of("first", "metadata"), payload_of("second", "metadata")])
 end
 
-
+# @param payload [RSocket::Payload]
 def fire_and_forget(payload)
   print "fire_and_forget"
 end
+
